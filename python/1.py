92d80a6e4ac0f6c54e1464b3a0510fa33866d6d0
李维 <503265298@qq.com>
2020-03-11 05:01:35 +0800

    222

--- python/1.py
+++ python/1.py
@@ -10,7 +10,7 @@ def register():
 	password=input("请输入密码：\n")
 
 	temp=username+'|'+password
-	with open('login.md','a') as f:
+	with open('login.md','w') as f:
 		f.write(temp)
 
 def login():
@@ -25,4 +25,26 @@ def login():
 		return True
 	else:
 		return False
-print(login())
+
+def getNick(func):
+	with open('login.md','r') as f:
+		info=f.read()
+	info=info.split('|')
+	if func:
+		print('{0}你好，欢迎访问无涯课堂'.format(info[0]))
+	else:
+		print('请登陆系统')
+
+if __name__=='__main__':
+	while True:
+		t=int(input('1、注册 2、登陆 3、退出\n'))
+		if t==1:
+			register()
+		elif t==2:
+			getNick(login())
+		elif t==3:
+			import sys
+			sys.exit(1)
+		else:
+			print('输入错误，请继续！')
+			continue
