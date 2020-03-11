d159eb6f057c41f4da44494553ec57028d456812
李维 <503265298@qq.com>
2020-03-11 17:22:58 +0800

    修改3.11

--- python/1.py
+++ python/1.py
@@ -1,49 +1,53 @@
 # def data(**kwargs):
 # 	return dict(sorted(kwargs.items(),key=lambda item:item[0]))
 # dict1={'维':'111','李':'2222'}
-#1111111
+# 1111111
 # print(data(name=1,pwd=2))
 
 def register():
 	'''实现账户的注册功能'''
-	username=input("请输入账号：\n")
-	password=input("请输入密码：\n")
+	username = input("请输入账号：\n")
+	password = input("请输入密码：\n")
 
-	temp=username+'|'+password
-	with open('login.md','w') as f:
+	temp = username + '|' + password
+	with open('login.md', 'w') as f:
 		f.write(temp)
+
 
 def login():
 	'''登陆函数'''
-	username=input("输入账号：\n")
-	password=input("输入密码：\n")
-	with open('login.md','r') as f:
-		info=f.read()
-	info=info.split('|')
+	username = input("输入账号：\n")
+	password = input("输入密码：\n")
+	with open('login.md', 'r') as f:
+		info = f.read()
+	info = info.split('|')
 	print(info)
-	if username==info[0] and password==info[1]:
+	if username == info[0] and password == info[1]:
 		return True
 	else:
 		return False
 
+
 def getNick(func):
-	with open('login.md','r') as f:
-		info=f.read()
-	info=info.split('|')
+	with open('login.md', 'r') as f:
+		info = f.read()
+	info = info.split('|')
 	if func:
 		print('{0}你好，欢迎访问无涯课堂'.format(info[0]))
 	else:
 		print('请登陆系统')
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
 	while True:
-		t=int(input('1、注册 2、登陆 3、退出\n'))
-		if t==1:
+		t = int(input('1、注册 2、登陆 3、退出\n'))
+		if t == 1:
 			register()
-		elif t==2:
+		elif t == 2:
 			getNick(login())
-		elif t==3:
+		elif t == 3:
 			import sys
+
 			sys.exit(1)
 		else:
 			print('输入错误，请继续！')
